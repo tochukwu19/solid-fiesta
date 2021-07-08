c06dbbb760cc20123483bd2215500755af443d45
Onwunali Tochukwu <Onwunali91@yahoo.com>
2021-07-08 11:43:27 +0100

    Final

--- src/pages/import1/import1.js
+++ src/pages/import1/import1.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import "./import1.css"
 import { useForm } from '@formspree/react';
-import { Link } from "react-router-dom"
 
 export default function Import(props) {
 
